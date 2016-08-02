024e74518397631a3d78a0878ffedb5cb58e1a7b
Matt Stuefen <mattstuefen@gmail.com>
2016-08-02 13:17:06 -0500

    Day 14

---
+++ DayFourteen/DayFourteen.py
@@ -0,0 +1,41 @@
+import re
+
+
+def get_leader_at_time(speed_data_string, time):
+    speed_data = parse_speed_data(speed_data_string)
+    position_data = {}
+
+    for racer in speed_data.keys():
+        position_data[racer] = get_distance_travelled(speed_data[racer], time)
+
+    leader = max(position_data, key=position_data.get)
+    return leader, position_data[leader]
+
+
+def parse_speed_data(speed_data_string):
+    res = {}
+    for line in speed_data_string.splitlines():
+        match = re.search("(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.", line)
+        racer = match.group(1)
+        res[racer] = {"speed": int(match.group(2)),
+                      "stamina": int(match.group(3)),
+                      "rest": int(match.group(4))}
+    return res
+
+def get_distance_travelled(racer_data, time):
+    cycle_time = racer_data["stamina"] + racer_data["rest"]
+    distance_per_cycle = racer_data["speed"] * racer_data["stamina"]
+
+    number_of_full_cycles = time / cycle_time
+    last_cycle_time = time % cycle_time
+    last_cycle_travel_time = racer_data["stamina"] if last_cycle_time > racer_data["stamina"] else last_cycle_time
+    last_cycle_distance = racer_data["speed"] * last_cycle_travel_time
+
+    return number_of_full_cycles * distance_per_cycle + last_cycle_distance
+
+
+test_input = "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\n" \
+             "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
+
+print "Example 1 test: " + str(get_leader_at_time(test_input, 1000))
+print "Final result: " + str(get_leader_at_time((open("./input")).read(), 2503))
