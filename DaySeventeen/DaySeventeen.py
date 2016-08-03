7b61979e2d0a6f88a868b509ad41d28470bcc765
Matt Stuefen <mattstuefen@gmail.com>
2016-08-02 20:27:43 -0500

    Day 17 Part 2

--- DaySeventeen/DaySeventeen.py
+++ DaySeventeen/DaySeventeen.py
@@ -1,7 +1,13 @@
+import ast
+
+
 def run(input_string, desired_total):
     containers_list = [int(line) for line in input_string.splitlines()]
     containers = dict([i, containers_list[i]] for i in range(len(containers_list)))
-    return find_container_sets(containers, desired_total)
+
+    container_sets = find_container_sets(containers, desired_total)
+
+    return len(container_sets), find_number_of_min_sets(container_sets)
 
 
 def find_container_sets(containers, desired_total, cur_sum=0, cur_container_set=[]):
@@ -21,5 +27,16 @@ def find_container_sets(containers, desired_total, cur_sum=0, cur_container_set=
     return unique_container_sets
 
 
-print "Example 1 test: " + str(len(run("20\n15\n10\n5\n5", 25)))
-print "Final result: " + str(len(run(open("./input").read(), 150)))
+def find_number_of_min_sets(container_sets):
+    # Convert sets from string to lists
+    container_sets = [ast.literal_eval(container_set) for container_set in container_sets]
+    # Find min length among all container sets
+    min_length = min([len(container_set) for container_set in container_sets])
+    # Create array of booleans where a True indicates that the length at that index of the container set is the minimum
+    min_length_array = [(len(container_set) == min_length) for container_set in container_sets]
+    # Sum boolean array (True = 1, False = 0)
+    return sum(min_length_array)
+
+
+print "Example 1 test: " + str(run("20\n15\n10\n5\n5", 25))
+print "Final result: " + str(run(open("./input").read(), 150))
